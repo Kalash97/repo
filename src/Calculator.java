4f55b2ce039400f728c902f835931b77dd90d164
Kalash97 <xvjdpl@wp.pl>
2019-07-30 21:37:30 +0200

    Added method dziel and created new branch dziel

--- src/Calculator.java
+++ src/Calculator.java
@@ -10,11 +10,17 @@ public class Calculator {
 		double wynik=a-b;
 		return wynik;
 	}
+	
+	public double dziel (double a, double b){
+		double wynik=a/b;
+		return wynik;
+	}
 
 	public static void main(String[] args) {
 		Calculator c = new Calculator();
 		System.out.println("dodawanie:" + c.dodaj(1,2));
 		System.out.println("odejmowanie" + c.odejmij(1,2));
+		System.out.println("dzielenie" + c.dziel(1,2));
 	}
 
 }
