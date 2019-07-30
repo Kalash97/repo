923843667dc0bb150a8389ff9f454d7c308fc9e9
Kalash97 <xvjdpl@wp.pl>
2019-07-30 21:34:01 +0200

    Added method mnoz and created branch mnoz

--- src/Calculator.java
+++ src/Calculator.java
@@ -10,11 +10,17 @@ public class Calculator {
 		double wynik=a-b;
 		return wynik;
 	}
+	
+	public double mnoz (double a, double b){
+		double wynik=a*b;
+		return wynik;
+	}
 
 	public static void main(String[] args) {
 		Calculator c = new Calculator();
 		System.out.println("dodawanie:" + c.dodaj(1,2));
 		System.out.println("odejmowanie" + c.odejmij(1,2));
+		System.out.println("mnozenie" + c.mnoz(1,2));
 	}
 
 }
